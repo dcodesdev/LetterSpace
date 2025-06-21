c720a5834febc208ab52722d4e903ccfe42b0043
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-06-21 20:32:34 +0300

    sql generated

--- apps/backend/prisma/client/sql/$DbEnums.d.ts
+++ apps/backend/prisma/client/sql/$DbEnums.d.ts
@@ -1,19 +1,8 @@
 export interface $DbEnums {}
 
+
 export namespace $DbEnums {
-  type CampaignStatus =
-    | "DRAFT"
-    | "SCHEDULED"
-    | "SENDING"
-    | "COMPLETED"
-    | "CANCELLED";
-  type MessageStatus =
-    | "QUEUED"
-    | "PENDING"
-    | "SENT"
-    | "OPENED"
-    | "CLICKED"
-    | "FAILED"
-    | "RETRYING";
-  type SmtpEncryption = "STARTTLS" | "SSL_TLS" | "NONE";
+  type CampaignStatus = "DRAFT" | "SCHEDULED" | "CREATING" | "SENDING" | "COMPLETED" | "CANCELLED"
+  type MessageStatus = "QUEUED" | "PENDING" | "SENT" | "AWAITING_WEBHOOK" | "OPENED" | "CLICKED" | "FAILED" | "RETRYING" | "CANCELLED" | "COMPLAINED"
+  type SmtpEncryption = "STARTTLS" | "SSL_TLS" | "NONE"
 }
